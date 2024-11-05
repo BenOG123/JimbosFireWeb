5faa04394b07ecf47bb449e952bcbca994e410aa
BenOG123 <benog1425@gmail.com>
2024-11-05 11:15:52 -0500

    Create basic-function.js

---
+++ basic-function.js
@@ -0,0 +1,6 @@
+var msg = 'Join the discord Now and get priority responses on my videos!';
+function updateMessage() {
+  var el = document.getElementById('message');
+  el.textContent = msg;
+}
+updateMessage();
